109a28e0fe2d594e64217897dce994af15fffb9b
Roel Spilker <r.spilker@gmail.com>
2020-06-08 14:48:30 +0200

    metric/imperial modifications in en and fr

--- docs/rules/keep_6ft_distance.js
+++ docs/rules/keep_6ft_distance.js
@@ -4,8 +4,8 @@ export default {
 	  icon: "keep_6_feet",
 	  lang: {
 	    ar: "ابق بعد 6 أقدام أو 1،5 م",
-	    en: "Keep 6 feet apart",
-	    fr: "Rester à 6 pieds l'un de l'autre",
+	    en: "Keep 6 feet (1.5 meters) apart",
+	    fr: "Rester à 1,5 mètre (6 pieds) l'un de l'autre",
 	    hu: "Tartson 1,5 méter távolságot!",
 	    nl: "Houd 1,5m afstand",
 	    tr: "Aranizda 1,5 metre mesafe bulundurun",
