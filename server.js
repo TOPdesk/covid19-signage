f605f556a8facd393701c8d92b1dfdfec0ab565b
Roel Spilker <r.spilker@gmail.com>
2020-07-01 22:56:48 +0200

    add securuty headers

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ import serveStatic from "serve-static";
 const serve = serveStatic("docs", { "index": ["index.html"] });
 
 const server = http.createServer((req, res) => {
+  res.setHeader("x-content-type-options", "nosniff");
+  res.setHeader("x-xss-protection", "1; mode=block");
+  res.setHeader("x-frame-options", "DENY");
+  res.setHeader("strict-transport-security", "max-age=31536000; includeSubDomains; preload");
+  res.setHeader("referrer-policy", "no-referrer");
   serve(req, res, finalhandler(req, res))
 })
 
